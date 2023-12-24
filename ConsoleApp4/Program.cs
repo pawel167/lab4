c517f6d2d72c79ae5632981db9fedd7c2034a9c9
pavel ozerov <brauber1@yandex.ru>
2023-12-24 23:14:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double k = double.Parse(Console.ReadLine());
+            double s = 1;
+            for (double i = 1; i <= k + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    s *= i / (i + 1);
+                }
+                else
+                {
+                    s *= (i + 1) / i;
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
